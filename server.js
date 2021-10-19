4376eb9699c353f1886953a26718901250b640f4
AiramVieira <leonardo.airam12@gmail.com>
2021-10-19 11:32:29 -0300

    .

--- server.js
+++ server.js
@@ -4,9 +4,26 @@ const express = require("express");
 
 const app = express();
 
+const path = require("path");
+
 // This is good for local dev environments, when it's better to
 // store a projects environment variables in a .gitignore'd file
 require("dotenv").config();
+
+//  Would be passed to script like this:
+// `ts-node set-env.ts --environment=dev`
+// we get it from yargs's argv object
+
+// HTTPS only middleware
+const forceSSL = function() {
+  return function(req, res, next) {
+    if (req.headers["x-forwarded-proto"] !== "https") {
+      return res.redirect(["https://", req.get("Host"), req.url].join(""));
+    }
+    next();
+  };
+};
+app.use(forceSSL());
 
 app.use(express.static(__dirname + "/dist"));
 
